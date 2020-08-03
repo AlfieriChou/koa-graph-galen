dc6efccc439de2f0279ea1fa3d342e926ab9cb11
AlfieriChou <alfierichou@gmail.com>
2020-08-04 00:42:02 +0800

    feat: users query

--- router.js
+++ router.js
@@ -6,6 +6,22 @@ router
   .get('/', async (ctx) => {
     ctx.body = 'Hello World'
   })
+  .get('/users', async (ctx) => {
+    const { query: { name } } = ctx
+    const res = await ctx.dgraphClinet.newTxn({ readOnly: true })
+      .queryWithVars(`query all($a: string) {
+        all(func: eq(name, $a))
+        {
+          friend {
+            name
+            age
+          }
+          age
+          name
+        }
+      }`, { $a: name })
+    ctx.body = res.getJson()
+  })
   .post('/users', async (ctx) => {
     const { name, age, friends } = ctx.request.body
     const txn = ctx.dgraphClinet.newTxn()
